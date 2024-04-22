34a9d963bf3849f9aea5f29e7afc9c2e10aa27e2
Christopher Perez <christopher.perez19kdc@gmail.com>
2024-04-21 21:49:31 -0500

    Created a database

--- EntityFrameworkWebApiShopOnline.Repositories/Models/Cart.cs
+++ EntityFrameworkWebApiShopOnline.Repositories/Models/Cart.cs
@@ -2,8 +2,8 @@
 {
     public class Cart
     {
-        public int id { get; set; }
-        public  int userId  { get; set; }
+        public int Id { get; set; }
+        public  int UserId  { get; set; }
 
     }
 }
